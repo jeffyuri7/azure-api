6b6c8b6b969f18f97177e599b24dad9718dbd784
Jefferson Yuri <jeffyuri@hotmail.com>
2025-01-12 14:06:10 -0300

    Azure Deploy API

--- Program.cs
+++ Program.cs
@@ -1,17 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.Dominio.Servicos;
+using MinimalApi.DTOs;
+using MinimalApi.Infraestrutura.Db;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.MapPost(
     "/login",
-    (MinimalApi.DTOs.LoginDTO loginDTO) =>
+    ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
     {
-        if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+        if (administradorServico.Login(loginDTO) != null)
             return Results.Ok("Login com sucesso");
         else
             return Results.Unauthorized();
     }
 );
 
+app.UseSwagger();
+app.UseSwaggerUI();
 app.Run();
